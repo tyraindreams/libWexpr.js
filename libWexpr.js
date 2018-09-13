8cdfee5528691ca747f1dd50c444be4706a024d8
Kenneth Perry (thothonegan) <thothonegan@gmail.com>
2018-09-12 22:20:41 -0500

    Put bareword check back inline

--- libWexpr.js
+++ libWexpr.js
@@ -319,14 +319,13 @@ libWexpr = {
                 return String(value);
             case "string":
                 str = String(value);
-                isBarewordCompatible = (str.match(/[^<>"*#@\(\);\[\]\r\n \t]+/) == str); // must match the entire string
                 if ( binary[path] != undefined ) {
                     if (Object.prototype.toString.call(typeof process !== 'undefined' ? process : 0) === '[object process]') {
                         return "<" + Buffer(str).toString('base64') + ">";
                     } else {
                         return "<" + window.btoa(str) + ">";
                     }
-                }else if (str != "nil" && str != "null" && str != "true" && str != "false" && isBarewordCompatible) {
+                }else if (str != "nil" && str != "null" && str != "true" && str != "false" && (str.match(/[^<>"*#@\(\);\[\]\r\n \t]+/) == str)) {
                     return str;
                 } else {
                     str = str.replace(/\\/g, "\\\\");
